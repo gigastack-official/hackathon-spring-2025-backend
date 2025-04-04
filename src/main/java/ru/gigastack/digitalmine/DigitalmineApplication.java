4c0b4dedc98e6e91e043f56006bf9a8772ce31df
Vladimir Gorohov <vova-gorohov04@mail.ru>
2025-04-04 16:56:04 +0700

    Инициализировал проект

---
+++ src/main/java/ru/gigastack/digitalmine/DigitalmineApplication.java
@@ -0,0 +1,13 @@
+package ru.gigastack.digitalmine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalmineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalmineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gigastack/digitalmine/DigitalmineApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gigastack.digitalmine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalmineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
