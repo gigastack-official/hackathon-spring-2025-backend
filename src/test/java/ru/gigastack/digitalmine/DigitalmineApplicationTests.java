50d6309b24979b477d9e193698fc4ccd1a66eadb
Vladimir Gorohov <vova-gorohov04@mail.ru>
2025-04-04 18:03:42 +0700

    Убрал тестировние

--- src/test/java/ru/gigastack/digitalmine/DigitalmineApplicationTests.java
+++ src/test/java/ru/gigastack/digitalmine/DigitalmineApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DigitalmineApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
 }
